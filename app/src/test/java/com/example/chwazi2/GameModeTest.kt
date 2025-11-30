d421e4b719c57991d8ea1a2c02510738d153c99e
Benoit Moriceau <benmoriceau@gmail.com>
2025-11-29 17:29:55 -0800

    Add tests

---
+++ app/src/test/java/com/example/chwazi2/GameModeTest.kt
@@ -0,0 +1,21 @@
+package com.example.chwazi2
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GameModeTest {
+
+    @Test
+    fun `verify game mode display names`() {
+        assertEquals("Starting Player", GameMode.STARTING_PLAYER.displayName)
+        assertEquals("Group", GameMode.GROUP.displayName)
+    }
+
+    @Test
+    fun `verify game mode values`() {
+        val modes = GameMode.values()
+        assertEquals(2, modes.size)
+        assertEquals(GameMode.STARTING_PLAYER, modes[0])
+        assertEquals(GameMode.GROUP, modes[1])
+    }
+}
